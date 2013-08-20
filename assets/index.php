d33d43f0c5dac6afe841830a8aad355107858f1e
Curtis LaMalfa <curtis@kmni.com>
2013-08-20 13:58:09 -0700

    Initial Commit

---
+++ assets/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Shortview Export</title>
+        <meta charset="utf-8" />
+        <link rel="Shortcut Icon" type="image/ico" href="images/favicon.png" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/cycle.js"></script>
+        <script type="text/javascript" src="js/theme.js"></script>
+        <script type="text/javascript" src="js/rounded.js"></script>
+        <!--[if IE 7]><link rel="stylesheet" href="assets/css/ie/ie7.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 7]><link rel="stylesheet" href="assets/css/ie/ie6.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 6]><script type="text/javascript">DD_roundies.addRule('.pngfix');</script><![endif]-->
+    </head>
+    <body>
+
+<div id="header-bg">
+            <div class="wrapper">
+                <div class="header">
+
+
+                    <div class="logo">
+
+                    </div><!--/logo-->
+                    <div class="navigation clearfix">
+                        <div class="navigation-links floatleft">
+
+
+                            <ul>
+                                <li><a href="#" title="" >Home</a></li>
+                                <li><a href="#" title="" >Exporting</a></li>
+                                <li><a href="#" title="" >About us</a></li>
+                                <li><a href="#" title="" >Blog</a></li>
+                                <li><a href="#" title="" >Shortview Stud</a></li>
+                                <li><a href="#" title="" >Contact us</a></li>
+                            </ul>
+                        </div><!--/navigation-links-->
+                        <div class="language floatright">
+                            <a href="#" title="" >Change Language</a>
+                        </div><!--/language-->
+                    </div><!--/navigation-->
+                </div><!--/header-->
+            </div><!--/wrapper-->
+    </div><!--/header-->
+        <div id="middle-header">
+            <div class="wrapper">
+                <div class="middle-container clearfix">
+                    <div class="map-bg">                                                 
+                    </div><!--/map-bg-->
+                    <div id="tagline" class="floatleft">
+                        <h3>Shortview Export</h3>
+                        <h1>Official expoter of the AMHA</h1>
+                    </div>   
+                    <div class="mc-image-box">
+                        <div class="image">
+                            <img src="images/mc-pic.png" alt="" />
+                        </div><!--/image-->
+                    </div><!--/mc-image-box-->
+                </div><!--/middle-container-->
+            </div><!--/wrapper-->
+        </div><!--/middle-header-->
+       
+        <div id="content-holder">
+            <div class="wrapper">
+                <div class="content clearfix">
+                    <div class="left-content floatleft">
+                      <img src="images/image1.jpg" alt="" width="465" height="188" />
+                      <img src="images/image2.jpg" alt="" width="465" height="188" />
+                    </div><!--/left-content-->
+                    <div class="right-content floatright">
+                        <h3>Full Service Horse-Export:
+</h3>
+                        <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                        <h4>Our Complete Services include:</h4>
+                        <ul>
+                             <li><a href="#" title="" >Quarantine care</a></li>
+                             <li><a href="#" title="" >Testing and preparation of the health certificates and other required 
+  documents</a></li>
+  <li><a href="#" title="" >Endorsement by USDA</a></li>
+  <li><a href="#" title="" >Transport to the departure airport</a></li>
+  <li><a href="#" title="" >Air freight costs, handling, agent fees</a></li>
+  <li><a href="#" title="" >USDA and TDA charges at departure airport</a></li>
+  <li><a href="#" title="" >Halter & lead, blanket/sheet</a></li>
+                        </ul>
+                    </div><!--/right-content-->
+                    
+                </div><!---/content-->
+                <div id="footer" class="clearfix">
+                    <ul  class="floatleft">
+                        <li><a href="#" title="" >Home</a></li>
+                        <li><a href="#" title="" >Exporting</a></li>
+                        <li><a href="#" title="" >About Us</a></li>
+                        <li><a href="#" title="" >Blog</a></li>
+                        <li><a href="#" title="" >Contact us</a></li>
+                    </ul>
+                    <div class="copyright floatright">
+                        <p>&copy; Shortview Export | Designed by ArabHorse.com</p>
+                    </div><!--/copyright-->
+        </div><!--/footer-->
+
+            </div><!--/wrapper-->
+    </div><!--/content-holder-->
+        
+    </body>
+</html>
---
+++ assets/single.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Shortview Export</title>
+        <meta charset="utf-8" />
+        <link rel="Shortcut Icon" type="image/ico" href="images/favicon.png" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/cycle.js"></script>
+        <script type="text/javascript" src="js/theme.js"></script>
+        <script type="text/javascript" src="js/rounded.js"></script>
+        <!--[if IE 7]><link rel="stylesheet" href="assets/css/ie/ie7.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 7]><link rel="stylesheet" href="assets/css/ie/ie6.css" type="text/css" media="screen"><![endif]-->
+        <!--[if lt IE 6]><script type="text/javascript">DD_roundies.addRule('.pngfix');</script><![endif]-->
+    </head>
+<body>
+
+<div id="header-bg">
+            <div class="wrapper">
+                <div class="header">
+
+
+                    <div class="logo">
+
+                    </div><!--/logo-->
+                    <div class="navigation clearfix">
+                        <div class="navigation-links floatleft">
+
+
+                            <ul>
+                                <li><a href="#" title="" >Home</a></li>
+                                <li><a href="#" title="" >Exporting</a></li>
+                                <li><a href="#" title="" >About us</a></li>
+                                <li><a href="#" title="" >Blog</a></li>
+                                <li><a href="#" title="" >Shortview Stud</a></li>
+                                <li><a href="#" title="" >Contact us</a></li>
+                            </ul>
+                        </div><!--/navigation-links-->
+                        <div class="language floatright">
+                            <a href="#" title="" >Change Language</a>
+                        </div><!--/language-->
+                    </div><!--/navigation-->
+                </div><!--/header-->
+            </div><!--/wrapper-->
+</div><!--/header-->
+        <div id="middle-header">
+            <div class="wrapper">
+                <div class="middle-container">
+                    <div class="map-bg">
+
+                    </div><!--/map-bg-->
+                   
+                </div><!--/middle-container-->
+            </div><!--/wrapper-->
+        </div><!--/middle-header-->
+       
+    <div class="post">
+        <div class="wrapper">
+            <div class="entry">
+                <h5>Exporting:</h5>
+                <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                <p>
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                </p>
+                <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                <p>
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                </p>
+                <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                <p>
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                </p>
+                <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                <p>
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                </p>
+                <p>Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.</p>
+                <p>
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                    Exporting a horse requires many different areas of expertise and commitment. Your horse will arrive and be placed in our approved, inspected quarantine facility and then as required they will be tested for the different diseases in order to meet each country’s requirements.
+                </p>
+            </div><!--/entry-->
+             <div id="footer" class="clearfix">
+                <ul  class="floatleft">
+                    <li><a href="#" title="" >Home</a></li>
+                    <li><a href="#" title="" >Exporting</a></li>
+                    <li><a href="#" title="" >About Us</a></li>
+                    <li><a href="#" title="" >Blog</a></li>
+                    <li><a href="#" title="" >Contact us</a></li>
+                </ul>
+                <div class="copyright floatright">
+                    <p>&copy; Shortview Export | Designed by ArabHorse.com</p>
+                </div><!--/copyright-->
+        </div><!--/footer-->
+        </div><!--/wrapper-->
+             
+</div><!--/post-->
+        
+        
+</body>
+</html>
