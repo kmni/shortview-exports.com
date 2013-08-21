7be0cf35e0d8cb545b44208ed92e6e07a79308ae
freyhubert <info@hubertfrey.cz>
2013-08-21 16:07:39 +0200

    Slideshow added

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,6 +5,25 @@
  * @Privacy Policy & Terms of Use: http://www.netchillies.com/terms
  * @License Info: http://www.netchillies.com/license
  ***************************************************************************/
+
+var hju	= {
+	slides:	function(){
+		$('.slideshow').slides({
+        	width:				894,
+        	autoHeight:			true,
+        	preloadImage:		'/assets/images/ajax-loader.gif',
+        	generateNextPrev:	false,
+        	pagination:			false,
+        	generatePagination:	false,
+        	slideSpeed:			500,
+        	play:				4000
+		});
+	},
+	init:	function(){
+		this.slides();
+	}
+};
+
 $(document).ready(function(){
- 
-})
+	hju.init();
+});
